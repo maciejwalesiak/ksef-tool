921d930a50f468a70233607fe70a5d2f2578b457
Maciej Walesiak <mwalesiak@mwiz.pl>
2026-03-26 18:15:17 +0100

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # ksef-tool
-A tool for generating and managing ksef invoices
+A tool for generating ksef invoice FA (3) xml file based on input invoice descriptor json.
+
+requirements: rust development environment and tools installed, including cargo
+
+build: ```cargo build```
+
+run: ```./target/debug/ksef-tool </path/to/invoice_data.json>```
+
+Example of invoice descriptor json:  
+
+```
+```json
+{
+  "number": "FV-01-01-26",
+  "currency": "PLN",
+  "seller": {
+    "nip": "1234567890",
+    "name": "company name",
+    "address": {
+      "country_code": "PL",
+      "street": "ulica",
+      "building_number": "1",
+      "flat_number": "2",
+      "city": "Warszawa",
+      "postal_code": "00 - 000"
+    }
+  },
+  "buyer": {
+    "nip": "0987654321",
+    "name": "firma z o.o.",
+    "address": {
+      "country_code": "PL",
+      "street": "inna ulica",
+      "building_number": "11",
+      "city": "Warszawa",
+      "postal_code": "00 - 003"
+    }
+  },
+  "positions": [
+    {
+      "name": "Usługi najlepsze",
+      "count": 16,
+      "price": 19.00,
+      "tax_rate": "23"
+    }
+  ],
+  "payment_details": {
+    "bank_name": "Bank Hajsowy",
+    "account_number": "10 10 1010 1010101",
+    "swift": "SIWFTW",
+    "period": 5
+  }
+}
+```
+```
+```
+```
---
+++ src/main.rs
@@ -0,0 +1,161 @@
+use invoice_gen::{
+    fa_3::{
+        builder::{BuyerBuilder, LineBuilder, SellerBuilder},
+        models::{BankAccount, Header, Invoice, InvoiceBody, Payment, PaymentTerm},
+    },
+    shared::TaxRate,
+};
+use rust_decimal::Decimal;
+use serde::Deserialize;
+use std::{env, fs::File, io::Read};
+
+#[derive(Debug, Deserialize)]
+struct Address {
+    country_code: String,
+    street: String,
+    building_number: String,
+    flat_number: Option<String>,
+    city: String,
+    postal_code: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Subject {
+    nip: String,
+    name: String,
+    address: Address,
+}
+
+#[derive(Debug, Deserialize)]
+struct Position {
+    name: String,
+    count: Decimal,
+    price: Decimal,
+    tax_rate: TaxRate,
+}
+
+#[derive(Debug, Deserialize)]
+struct PaymentDetails {
+    bank_name: String,
+    account_number: String,
+    swift: Option<String>,
+    period: Option<i64>,
+}
+
+#[derive(Debug, Deserialize)]
+struct InvoiceData {
+    number: Option<String>,
+    currency: String,
+    seller: Subject,
+    buyer: Subject,
+    positions: Vec<Position>,
+    payment_details: Option<PaymentDetails>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} </path/to/invoice_data.json>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let invoice_data: InvoiceData = serde_json::from_str(&contents)?;
+    // println!("{:?}", invoice_data);
+
+    let invoice = Invoice {
+        header: Header {
+            system_info: None,
+            ..Default::default()
+        },
+        subject1: SellerBuilder::new(&invoice_data.seller.nip, &invoice_data.seller.name)
+            .set_address(
+                &invoice_data.seller.address.country_code,
+                &invoice_data.seller.address.street,
+                &invoice_data.seller.address.building_number,
+                invoice_data.seller.address.flat_number.as_deref(),
+                &invoice_data.seller.address.city,
+                &invoice_data.seller.address.postal_code,
+            )
+            .build(),
+        subject2: BuyerBuilder::new(&invoice_data.buyer.nip, &invoice_data.buyer.name)
+            .set_address(
+                &invoice_data.buyer.address.country_code,
+                &invoice_data.buyer.address.street,
+                &invoice_data.buyer.address.building_number,
+                invoice_data.buyer.address.flat_number.as_deref(),
+                &invoice_data.buyer.address.city,
+                &invoice_data.buyer.address.postal_code,
+            )
+            .build(),
+        invoice_body: InvoiceBody {
+            invoice_number: invoice_data.number.unwrap_or(
+                // TODO: generate incremental number based on last value from given month stored in
+                // db
+                "XX/XX/XX".to_string(),
+            ),
+            issue_date: chrono::Local::now().format("%Y-%m-%d").to_string(),
+            currency_code: invoice_gen::shared::models::CurrencyCode::new(invoice_data.currency),
+            lines: {
+                let mut lines = Vec::default();
+                for position in invoice_data.positions {
+                    lines.push(
+                        LineBuilder::new(
+                            &position.name,
+                            position.count,
+                            position.price,
+                            position.tax_rate,
+                        )
+                        .build(),
+                    );
+                }
+                lines
+            },
+            payment: invoice_data.payment_details.map(|payment_details| Payment {
+                bank_accounts: vec![BankAccount {
+                    account_number: payment_details.account_number,
+                    bank_name: Some(payment_details.bank_name),
+                    description: None,
+                    own_account: None,
+                    swift: payment_details.swift,
+                }],
+                paid: None,
+                payment_date: None,
+                partial_payment_flag: None,
+                partial_payments: Vec::default(),
+                payment_terms: payment_details
+                    .period
+                    .map(|period| {
+                        vec![PaymentTerm {
+                            date: Some(
+                                (chrono::Local::now() + chrono::TimeDelta::days(period))
+                                    .format("%Y-%m-%d")
+                                    .to_string(),
+                            ),
+                            description: None,
+                        }]
+                    })
+                    .unwrap_or(Vec::default()),
+                payment_method: Some(6),
+                other_payment: None,
+                payment_description: None,
+                factor_bank_accounts: Vec::default(),
+                discount: None,
+                payment_link: None,
+                ip_ksef: None,
+            }),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    //println!("{invoice:?}");
+    let xml = invoice.to_xml().unwrap();
+    println!("{xml}");
+
+    Ok(())
+}
